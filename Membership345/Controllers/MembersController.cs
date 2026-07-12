5346102d42998df275f61bf80740a7dfe677cda5
aribilgiogr <aribilgiogr@gmail.com>
2026-07-12 14:31:53 +0300

    Update 02 12.07.2026

--- Membership345/Controllers/MembersController.cs
+++ Membership345/Controllers/MembersController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Membership345.Data;
+using Membership345.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Membership345.Controllers
 {
     public class MembersController : Controller
     {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public MembersController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +25,80 @@ namespace Membership345.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else if (result.IsLockedOut)
+                {
+                    ModelState.AddModelError(string.Empty, "Hesap müsait değil!");
+                }
+                else if (result.IsNotAllowed)
+                {
+                    ModelState.AddModelError(string.Empty, "Hesap yetkisi bulunamadı!");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Eposta adresi veya şifre hatalı!");
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    DisplayName = model.DisplayName,
+                    Bio = model.Bio,
+                    City = model.City,
+                    Country = model.Country,
+                    DateOfBirth = model.DateOfBirth,
+                    District = model.District,
+                    Gender = model.Gender,
+                    ProfilePicture = model.ProfilePicture
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var err in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, err.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
         }
     }
 }
---
+++ Membership345/Models/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Membership345.Models
+{
+    public class RegisterViewModel
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string ConfirmPassword { get; set; } = null!;
+        public string? DisplayName { get; set; }
+        public string? ProfilePicture { get; set; }
+        public string? Bio { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+        public string Country { get; set; } = null!;
+        public string? City { get; set; }
+        public string? District { get; set; }
+    }
+}
